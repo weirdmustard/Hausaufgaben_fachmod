0d7cd258f6f352f71e385852ceac0e38d7294c92
weirdmustard <samael.schroeder@mail.de>
2024-04-28 08:35:34 +0200

    initial

---
+++ src/hausauf_fachmod/aufgaben.java
@@ -0,0 +1,10 @@
+package hausauf_fachmod;
+
+public class aufgaben {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module hausauf_fachmod {
+}
